e3ac9c30db83a694a16dfe833005f05941553dda
Tobixy <shivamrajput69bs@gmail.com>
2023-07-10 08:15:54 +0530

    Update __init__.py

--- RoroRobot/__init__.py
+++ RoroRobot/__init__.py
@@ -53,7 +53,7 @@ if sys.version_info[0] < 3 or sys.version_info[1] < 9:
 ENV = bool(os.environ.get("ENV", False))
 
 if ENV:
-    TOKEN = os.environ.get("TOKEN", None)
+    BOT_TOKEN = os.environ.get("TOKEN", None)
 
     try:
         OWNER_ID = int(os.environ.get("OWNER_ID", None))
@@ -149,7 +149,7 @@ if ENV:
 else:
     from RoroRobot.config import Development as Config
 
-    TOKEN = Config.TOKEN
+    BOT_TOKEN = Config.TOKEN
 
     try:
         OWNER_ID = int(Config.OWNER_ID)
