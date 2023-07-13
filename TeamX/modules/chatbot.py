b6bbd6936f36a7adb8d199ed30152eca7ffa300e
epic-designer <mohsinkhan1292002@gmail.com>
2023-07-13 18:59:34 +0530

    chatbot added

--- TeamX/modules/chatbot.py
+++ TeamX/modules/chatbot.py
@@ -1,169 +1,169 @@
+import html
 import json
 import re
-import os
-import html
-import requests
-import TeamX.modules.sql.chatbot_sql as sql
-from TeamX.modules.language import gs
-
 from time import sleep
-from telegram import ParseMode
-from telegram import (CallbackQuery, Chat, MessageEntity, InlineKeyboardButton,
-                      InlineKeyboardMarkup, Message, Update, Bot, User)
-from telegram.ext import (CallbackContext, CallbackQueryHandler, CommandHandler,
-                          DispatcherHandlerStop, Filters, MessageHandler,
-                          run_async)
-from telegram.error import BadRequest, RetryAfter, Unauthorized
-from telegram.utils.helpers import mention_html, mention_markdown, escape_markdown
 
-from TeamX.modules.helper_funcs.filters import CustomFilters
+import requests
+from telegram import (
+    CallbackQuery,
+    Chat,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    ParseMode,
+    Update,
+    User,
+)
+from telegram.ext import (
+    CallbackContext,
+    CallbackQueryHandler,
+    CommandHandler,
+    Filters,
+    MessageHandler,
+)
+from telegram.utils.helpers import mention_html
+
+import TeamX.modules.sql.chatbot_sql as sql
+from TeamX import BOT_ID, BOT_NAME, BOT_USERNAME, dispatcher
 from TeamX.modules.helper_funcs.chat_status import user_admin, user_admin_no_reply
-from TeamX import dispatcher, updater, SUPPORT_CHAT
 from TeamX.modules.log_channel import gloggable
+
 
 @user_admin_no_reply
 @gloggable
-def kukirm(update: Update, context: CallbackContext) -> str:
+def TeamXrm(update: Update, context: CallbackContext) -> str:
     query: Optional[CallbackQuery] = update.callback_query
     user: Optional[User] = update.effective_user
     match = re.match(r"rm_chat\((.+?)\)", query.data)
     if match:
         user_id = match.group(1)
         chat: Optional[Chat] = update.effective_chat
-        is_kuki = sql.rem_kuki(chat.id)
-        if is_kuki:
-            is_kuki = sql.rem_kuki(user_id)
+        is_TeamX = sql.set_TeamX(chat.id)
+        if is_TeamX:
+            is_TeamX = sql.set_TeamX(user_id)
             return (
                 f"<b>{html.escape(chat.title)}:</b>\n"
                 f"AI_DISABLED\n"
-                f"<b>Admin:</b> {mention_html(user.id, html.escape(user.first_name))}\n"
+                f"<b>Admin :</b> {mention_html(user.id, html.escape(user.first_name))}\n"
             )
         else:
             update.effective_message.edit_text(
-                "Chatbot disable by {}.".format(mention_html(user.id, user.first_name)),
+                "{} ᴄʜᴀᴛʙᴏᴛ ᴅɪsᴀʙʟᴇᴅ ʙʏ {}.".format(
+                    dispatcher.bot.first_name, mention_html(user.id, user.first_name)
+                ),
                 parse_mode=ParseMode.HTML,
             )
 
     return ""
 
+
 @user_admin_no_reply
 @gloggable
-def kukiadd(update: Update, context: CallbackContext) -> str:
+def TeamXadd(update: Update, context: CallbackContext) -> str:
     query: Optional[CallbackQuery] = update.callback_query
     user: Optional[User] = update.effective_user
     match = re.match(r"add_chat\((.+?)\)", query.data)
     if match:
         user_id = match.group(1)
         chat: Optional[Chat] = update.effective_chat
-        is_kuki = sql.set_kuki(chat.id)
-        if is_kuki:
-            is_kuki = sql.set_kuki(user_id)
+        is_TeamX = sql.rem_TeamX(chat.id)
+        if is_TeamX:
+            is_TeamX = sql.rem_TeamX(user_id)
             return (
                 f"<b>{html.escape(chat.title)}:</b>\n"
                 f"AI_ENABLE\n"
-                f"<b>Admin:</b> {mention_html(user.id, html.escape(user.first_name))}\n"
+                f"<b>Admin :</b> {mention_html(user.id, html.escape(user.first_name))}\n"
             )
         else:
             update.effective_message.edit_text(
-                "Chatbot enable by {}.".format(mention_html(user.id, user.first_name)),
+                "{} ᴄʜᴀᴛʙᴏᴛ ᴇɴᴀʙʟᴇᴅ ʙʏ {}.".format(
+                    dispatcher.bot.first_name, mention_html(user.id, user.first_name)
+                ),
                 parse_mode=ParseMode.HTML,
             )
 
     return ""
 
-CHATBOT_KEY = "1491497760-KUKIGz8LCr6rH3"
 
 @user_admin
 @gloggable
-def kuki(update: Update, context: CallbackContext):
-    user = update.effective_user
+def TeamX(update: Update, context: CallbackContext):
     message = update.effective_message
-    msg = "Choose an option"
-    keyboard = InlineKeyboardMarkup([[
-        InlineKeyboardButton(
-            text="Enable",
-            callback_data="add_chat({})")],
-       [
-        InlineKeyboardButton(
-            text="Disable",
-            callback_data="rm_chat({})")]])
+    msg = "• ᴄʜᴏᴏsᴇ ᴀɴ ᴏᴩᴛɪᴏɴ ᴛᴏ ᴇɴᴀʙʟᴇ/ᴅɪsᴀʙʟᴇ ᴄʜᴀᴛʙᴏᴛ"
+    keyboard = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(text="ᴇɴᴀʙʟᴇ", callback_data="add_chat({})"),
+                InlineKeyboardButton(text="ᴅɪsᴀʙʟᴇ", callback_data="rm_chat({})"),
+            ],
+        ]
+    )
     message.reply_text(
-        msg,
+        text=msg,
         reply_markup=keyboard,
         parse_mode=ParseMode.HTML,
     )
 
-def kuki_message(context: CallbackContext, message):
+
+def TeamX_message(context: CallbackContext, message):
     reply_message = message.reply_to_message
-    if message.text.lower() == "kuki":
+    if message.text.lower() == "TeamX":
         return True
-    if reply_message:
-        if reply_message.from_user.id == context.bot.get_me().id:
+    elif BOT_USERNAME in message.text.upper():
+        return True
+    elif reply_message:
+        if reply_message.from_user.id == BOT_ID:
             return True
     else:
         return False
-        
+
 
 def chatbot(update: Update, context: CallbackContext):
     message = update.effective_message
     chat_id = update.effective_chat.id
     bot = context.bot
-    is_kuki = sql.is_kuki(chat_id)
-    if not is_kuki:
+    is_TeamX = sql.is_fallen(chat_id)
+    if is_TeamX:
         return
-	
+
     if message.text and not message.document:
-        if not kuki_message(context, message):
+        if not TeamX_message(context, message):
             return
-        Message = message.text
         bot.send_chat_action(chat_id, action="typing")
-        kukiurl = requests.get(f'https://kukiapi.xyz/api/apikey=5148561602-KUKIdh4KjG9Xi1/kaguya/wade/message='+Message)
-        Kuki = json.loads(kukiurl.text)
-        kuki = Kuki['reply']
-        sleep(0.3)
-        message.reply_text(kuki, timeout=60)
+        request = requests.get(
+            f"https://api.safone.me/chatbot?query={message.text}&user_id={chat_id}&bot_name=Group_Controller&bot_master=Mukesh"
+        )
+        results = json.loads(request.text)
+        sleep(0.5)
+        message.reply_text(results["response"])
 
 
-def list_all_chats(update: Update, context: CallbackContext):
-    chats = sql.get_all_kuki_chats()
-    text = "<b>Vegeta Enabled Chats</b>\n"
-    for chat in chats:
-        try:
-            x = context.bot.get_chat(int(*chat))
-            name = x.title or x.first_name
-            text += f"• <code>{name}</code>\n"
-        except (BadRequest, Unauthorized):
-            sql.rem_kuki(*chat)
-        except RetryAfter as e:
-            sleep(e.retry_after)
-    update.effective_message.reply_text(text, parse_mode="HTML")
+__help__ = f"""
+*{BOT_NAME} ʜᴀs ᴀɴ ᴄʜᴀᴛʙᴏᴛ ᴡʜɪᴄʜ ᴘʀᴏᴠɪᴅᴇs ʏᴏᴜ ᴀ sᴇᴇᴍɪɴɢʟᴇss ᴄʜᴀᴛᴛɪɴɢ ᴇxᴘᴇʀɪᴇɴᴄᴇ :**
 
-def helps(chat):
-    return gs(chat, "chatbot_help")
+ »  /ᴄʜᴀᴛʙᴏᴛ *:* sʜᴏᴡs ᴄʜᴀᴛʙᴏᴛ ᴄᴏɴᴛʀᴏʟ ᴘᴀɴᴇʟ
+"""
+
+__mod_name__ = "Cʜᴀᴛʙᴏᴛ"
 
 
-__mod_name__ = "ChatBot"
-
-
-CHATBOTK_HANDLER = CommandHandler("chatbot", kuki,run_async=True)
-ADD_CHAT_HANDLER = CallbackQueryHandler(kukiadd, pattern=r"add_chat",run_async=True)
-RM_CHAT_HANDLER = CallbackQueryHandler(kukirm, pattern=r"rm_chat",run_async=True)
+CHATBOTK_HANDLER = CommandHandler("chatbot", TeamX, run_async=True)
+ADD_CHAT_HANDLER = CallbackQueryHandler(TeamXadd, pattern=r"add_chat", run_async=True)
+RM_CHAT_HANDLER = CallbackQueryHandler(TeamXrm, pattern=r"rm_chat", run_async=True)
 CHATBOT_HANDLER = MessageHandler(
-    Filters.text & (~Filters.regex(r"^#[^\s]+") & ~Filters.regex(r"^!")
-                    & ~Filters.regex(r"^\/")), chatbot, )
-LIST_ALL_CHATS_HANDLER = CommandHandler(
-    "allchats", list_all_chats, filters=CustomFilters.dev_filter, )
+    Filters.text
+    & (~Filters.regex(r"^#[^\s]+") & ~Filters.regex(r"^!") & ~Filters.regex(r"^\/")),
+    chatbot,
+    run_async=True,
+)
 
 dispatcher.add_handler(ADD_CHAT_HANDLER)
 dispatcher.add_handler(CHATBOTK_HANDLER)
 dispatcher.add_handler(RM_CHAT_HANDLER)
-dispatcher.add_handler(LIST_ALL_CHATS_HANDLER)
 dispatcher.add_handler(CHATBOT_HANDLER)
 
 __handlers__ = [
     ADD_CHAT_HANDLER,
     CHATBOTK_HANDLER,
     RM_CHAT_HANDLER,
-    LIST_ALL_CHATS_HANDLER,
     CHATBOT_HANDLER,
 ]
