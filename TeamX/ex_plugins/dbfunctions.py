676dc6e497e1df5ee821c36fd2f784f9e91e44b2
epic-designer <mohsinkhan1292002@gmail.com>
2023-07-13 20:34:00 +0530

    fixing couples

--- TeamX/ex_plugins/dbfunctions.py
+++ TeamX/ex_plugins/dbfunctions.py
@@ -410,6 +410,31 @@ async def remove_gban_user(user_id: int):
         return
     return gbansdb.delete_one({"user_id": user_id})
 
+
+async def _get_lovers(chat_id: int):
+    lovers = coupledb.find_one({"chat_id": chat_id})
+    if not lovers:
+        return {}
+    return lovers["couple"]
+
+
+async def get_couple(chat_id: int, date: str):
+    lovers = await _get_lovers(chat_id)
+    if date in lovers:
+        return lovers[date]
+    return False
+
+
+async def save_couple(chat_id: int, date: str, couple: dict):
+    lovers = await _get_lovers(chat_id)
+    lovers[date] = couple
+    coupledb.update_one(
+        {"chat_id": chat_id},
+        {"$set": {"couple": lovers}},
+        upsert=True,
+    )
+
+
 async def is_captcha_on(chat_id: int) -> bool:
     chat = captchadb.find_one({"chat_id": chat_id})
     if not chat:
@@ -750,31 +775,3 @@ async def get_rss_feeds_count() -> int:
     feeds = rssdb.find({"chat_id": {"$exists": 1}})
     feeds = await feeds.to_list(length=10000000)
     return len(feeds)
-
-
-
-
-def _get_lovers(chat_id: int):
-    lovers = coupledb.find_one({"chat_id": chat_id})
-    if not lovers:
-        return {}
-    return lovers["couple"]
-
-
-def get_couple(chat_id: int, date: str):
-    lovers =  _get_lovers(chat_id)
-    if date in lovers:
-        return lovers[date]
-    return False
-
-def del_couple(chat_id : int):
-    lovers =  coupledb.find_one({"chat_id": chat_id})
-    if lovers :
-        return  coupledb.delete_one({"chat_id": chat_id})
-
-def save_couple(chat_id: int, date: str, couple: dict):
-    lovers =  _get_lovers(chat_id)
-    lovers[date] = couple
-    coupledb.update_one(
-        {"chat_id": chat_id}, {"$set": {"couple": lovers}}, upsert=True
-    )
