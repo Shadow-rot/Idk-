78097657d19f867391a4bb756cbf4fee404474f0
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-07-14 17:55:44 +0530

    Update global_bans_sql.py
    Don't deploy

--- TeamX/modules/sql/global_bans_sql.py
+++ TeamX/modules/sql/global_bans_sql.py
@@ -1,12 +1,12 @@
 import threading
 from TeamX.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, String, UnicodeText
-#from sqlalchemy.sql.sqltypes import Integer , Numeric
+from sqlalchemy.sql.sqltypes import BigInteger
 
 
 class GloballyBannedUsers(BASE):
     __tablename__ = "gbans"
-    user_id = Column(String, primary_key=True)
+    user_id = Column(BigInteger, primary_key=True)
     name = Column(UnicodeText, nullable=False)
     reason = Column(UnicodeText)
 
